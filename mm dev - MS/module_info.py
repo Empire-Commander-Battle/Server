8d721b9b06fbc6c334c16c62c44d39449973f343
Bill Gats <fuck@microsoft.com>
2024-01-20 01:35:02 +0100

    'Initial'

---
+++ mm dev - MS/module_info.py
@@ -0,0 +1,12 @@
+
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+# TESTING
+export_dir = "C:/Users/user/Downloads/Mount&Blade Warband Napoleonic Wars Dedicated/Modules/Napoleonic Wars/"
+
+# PROD
+#export_dir = "Z:\Mount&Blade Warband Napoleonic Wars Dedicated\Modules\Napoleonic Wars/"
+
+#export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
+#export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Napoleonic Wars Patch/"
